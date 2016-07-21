b902e9261401891353938035ed3740b6c99bd5e6
Titouan Bion <titouan.bion@gmail.com>
2016-07-21 15:26:05 +0200

    fix: script now handling mongodb remote

--- evaluation/evaluation_script.sh
+++ evaluation/evaluation_script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# 1. Create ProgressBar function
-# 1.1 Input is currentState($1) and totalState($2)
 ProgressBar() {
-# Process data
     _progress_percent=$(($((${1}*100/${2}*100))/100))
     _done=$((${_progress_percent}*4/10))
     _left=$((40-${_done}))
@@ -15,7 +12,7 @@ ProgressBar() {
 
 launch() {
     # Launching benchmark
-    output="$(${4}bin/ycsb ${2} memcached -P ${4}workloads/${3} -p measurementtype=${1} -p threadcount=${6} -p frontend.collection.benchmark=${5} -p memcached.hosts=${7} -s)"
+    output="$(${4}bin/ycsb ${2} memcached -P ${4}workloads/${3} -p measurementtype=${1} -p threadcount=${6} -p frontend.collection.benchmark=${5} -p memcached.hosts=${7} -p frontend.db.uri=mongodb://${8}/ -p frontend.db.name=${9} -p frontend.collection.counters=${10} -s)"
     res="$(echo ${output} | sed 's/.* Throughput(ops\/sec), \([0-9.,]*\).*/\1/g')"
 }
 
@@ -23,9 +20,16 @@ launch() {
 iterations=${1}
 thread_count=${2}
 workload=${3}
-memcached_address=${4}
+memcached_host=${4}
+memcached_address=$(echo ${memcached_host} | cut -f1 -d:)
+memcached_port=$(echo ${memcached_host} | cut -f2 -d:)
+storage_uri=${5}
+
+# Custom parameters
+storage_db_name="dbMeasurements"
+storage_counters_collection_name="counters"
 ycsb_root="/home/titouan/Documents/ycsb-web-app/ycsb-0.11.1-custom-release/"
-unique_id="results-I${1}-T${2}-W${3}-M${4}"
+unique_id="results-I${1}-W${3}-M${4}-T${2}-S${5}"
 output_file="/home/titouan/Documents/ycsb-web-app/public/evaluations/${unique_id}.json"
 
 
@@ -45,19 +49,27 @@ echo "{" > ${output_file}
 echo "    \"data\": [" >> ${output_file}
 while [ "$i" -lt "$adjusted_iterations" ]
  do
-    FRONTEND_CYCLE_START=$(date +%s.%N)
     # Preparing memcached environment
-    echo 'flush_all' | nc ${memcached_address} 11211  >> /dev/null 2>&1
+    echo 'flush_all' | nc ${memcached_address} ${memcached_port} >> /dev/null 2>&1
 
-    launch frontend load ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_address} >> /dev/null 2>&1
+    FRONTEND_CYCLE_START=$(date +%s.%N)
+
+    launch frontend load ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_host} ${storage_uri} ${storage_db_name} ${storage_counters_collection_name} >> /dev/null 2>&1
     current_res_frontend_load=$res
     res_frontend_load=$(echo "$res_frontend_load + $res" | bc)
 
     ProgressBar $(($i + 5)) ${total_adjusted_iterations}
 
-    launch frontend run ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_address} >> /dev/null 2>&1
+    launch frontend run ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_host} ${storage_uri} ${storage_db_name} ${storage_counters_collection_name} >> /dev/null 2>&1
     current_res_frontend_run=$res
     res_frontend_run=$(echo "$res_frontend_run + $res" | bc)
+
+    FRONTEND_CYCLE_END=$(date +%s.%N)
+    FRONTEND_EXECUTION_TIME=$(echo "$FRONTEND_CYCLE_END - $FRONTEND_CYCLE_START" | bc)
+
+    # Cleaning up storage DB
+    mongo "${storage_uri}/${storage_db_name}" --eval "db[\"${unique_id}bash${i}\"].drop()" >> /dev/null 2>&1
+    mongo "${storage_uri}/${storage_db_name}" --eval "db[\"${storage_counters_collection_name}\"].remove({\"collection\":\"${unique_id}bash${i}\"})" >> /dev/null 2>&1
 
     i=$(($i + 10))
     ProgressBar ${i} ${total_adjusted_iterations}
@@ -68,29 +80,28 @@ while [ "$i" -lt "$adjusted_iterations" ]
     echo "            \"load\": $current_res_frontend_load," >> ${output_file}
     echo "            \"run\": $current_res_frontend_run" >> ${output_file}
     echo "        }," >> ${output_file}
-
-    FRONTEND_CYCLE_END=$(date +%s.%N)
-    FRONTEND_EXECUTION_TIME=$(echo "$FRONTEND_CYCLE_END - $FRONTEND_CYCLE_START" | bc)
 done
-average_frontend_load=$(echo "$res_frontend_load / ${iterations}" | bc)
-average_frontend_run=$(echo "$res_frontend_run / ${iterations}" | bc)
 
 # RAW CYCLE
 while [ "$i" -lt "${total_adjusted_iterations}" ]
  do
-    RAW_CYCLE_START=$(date +%s.%N)
- # Preparing memcached environment
-    echo 'flush_all' | nc ${memcached_address} 11211 >> /dev/null 2>&1
+    # Preparing memcached environment
+    echo 'flush_all' | nc ${memcached_address} ${memcached_port} >> /dev/null 2>&1
 
-    launch raw load ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_address} >> /dev/null 2>&1
+    RAW_CYCLE_START=$(date +%s.%N)
+
+    launch raw load ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_host} ${storage_uri} ${storage_db_name} ${storage_counters_collection_name} >> /dev/null 2>&1
     current_res_raw_load=$res
     res_raw_load=$(echo "$res_raw_load + $res" | bc)
 
     ProgressBar $(($i + 5)) ${total_adjusted_iterations}
 
-    launch raw run ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_address} >> /dev/null 2>&1
+    launch raw run ${workload} ${ycsb_root} ${unique_id}bash${i} ${thread_count} ${memcached_host} ${storage_uri} ${storage_db_name} ${storage_counters_collection_name} >> /dev/null 2>&1
     current_res_raw_run=$res
     res_raw_run=$(echo "$res_raw_run + $res" | bc)
+
+    RAW_CYCLE_END=$(date +%s.%N)
+    RAW_EXECUTION_TIME=$(echo "$RAW_CYCLE_END - $RAW_CYCLE_START" | bc)
 
     i=$(($i + 10))
     ProgressBar ${i} ${total_adjusted_iterations}
@@ -107,10 +118,10 @@ while [ "$i" -lt "${total_adjusted_iterations}" ]
     else
         echo "        }" >> ${output_file}
     fi
-
-    RAW_CYCLE_END=$(date +%s.%N)
-    RAW_EXECUTION_TIME=$(echo "$RAW_CYCLE_END - $RAW_CYCLE_START" | bc)
 done
+
+average_frontend_load=$(echo "$res_frontend_load / ${iterations}" | bc)
+average_frontend_run=$(echo "$res_frontend_run / ${iterations}" | bc)
 average_raw_load=$(echo "$res_raw_load / ${iterations}" | bc)
 average_raw_run=$(echo "$res_raw_run / ${iterations}" | bc)
 
@@ -133,8 +144,8 @@ echo "            \"raw\": ${RAW_EXECUTION_TIME}," >> ${output_file}
 echo "            \"frontend\": ${FRONTEND_EXECUTION_TIME}" >> ${output_file}
 echo "        }," >> ${output_file}
 echo "        \"percents\": {" >> ${output_file}
-echo "            \"average_run\": ${percent_average_load}," >> ${output_file}
-echo "            \"average_load\": ${percent_average_run}," >> ${output_file}
+echo "            \"average_load\": ${percent_average_load}," >> ${output_file}
+echo "            \"average_run\": ${percent_average_run}," >> ${output_file}
 echo "            \"execution_time\": ${percent_execution_time}" >> ${output_file}
 echo "        }" >> ${output_file}
 echo "    }" >> ${output_file}
--- evaluation/evaluation_sequence.sh
+++ evaluation/evaluation_sequence.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-MEMCACHED_ADDRESSES=( 127.0.0.1 141.76.44.145 )
-WORKLOADS=( workloada workloadaR workloada100k workloadaR100k workloada1M workloadaR1M workloada10M workloadaR10M )
+
+ITERATION=2
+
+MEMCACHED_URIS=( 127.0.0.1:11211 141.76.44.145:11211 )
+MONGO_URIS=( 127.0.0.1:27017 141.76.44.145:27017 )
+WORKLOADS=( a1k a100k a1M a10M )
 THREADS=( 1 4 )
-MEASUREMENTS=( raw )
+MEASUREMENTS=( raw ) # No other measurement supported at the moment
 
 for workload in ${WORKLOADS[@]}; do
-    for address in ${MEMCACHED_ADDRESSES[@]}; do
-        for thread in ${THREADS[@]}; do
-            for measurement in ${MEASUREMENTS[@]}; do
-                echo "Launching : 100 ${thread} ${workload} ${address}"
-                ./evaluation_script.sh 100 ${thread} ${workload} ${address}
+    for mongo_uri in ${MONGO_URIS[@]}; do
+        for memcached_uri in ${MEMCACHED_URIS[@]}; do
+            for thread in ${THREADS[@]}; do
+                for measurement in ${MEASUREMENTS[@]}; do
+                    echo "\nLaunching : ${ITERATION} ${thread} ${workload} ${memcached_uri} ${mongo_uri}"
+                    ./evaluation_script.sh ${ITERATION} ${thread} ${workload} ${memcached_uri} ${mongo_uri}
+                done
             done
         done
     done
